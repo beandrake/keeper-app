33c5fdf05a427bb129b3f6be3daa25d34a510664
Branden Bean Drake <30214543+beandrake@users.noreply.github.com>
2024-12-13 16:11:11 -0800

    Completed the first stage of this project, giving it a basic outline.  Also made some alterations to package.json so that I could use React 18's createRoot rather than the deprecated ReactDom render.

--- src/index.jsx
+++ src/index.jsx
@@ -16,3 +16,12 @@
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+
+const container = document.getElementById('root')
+const root = createRoot(container);
+
+root.render(<App />);
+
