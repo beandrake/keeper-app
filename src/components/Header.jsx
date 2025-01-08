ab7858d0ec48f1b7c81d10a55a7999585a3558fe
Branden Bean Drake <30214543+beandrake@users.noreply.github.com>
2025-01-08 11:04:07 -0800

    Added some hooks, just because I could.  These will probably be removed later in favor of meaningful hooks.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 function Header() {
+	const [minorVersion, setMinorVersion] = React.useState(0);
+	
+	function increaseMinorVersion() {
+		setMinorVersion(minorVersion + 1);
+	}
+	function decreaseMinorVersion() {
+		setMinorVersion(minorVersion > 0 ? minorVersion - 1 : 0);
+	}
+
 	return (
 		<header>
-			<h1>Keeper</h1>
+			<h1>Keeper - v1.{minorVersion}</h1>
+			<div>
+				<button onMouseDown={decreaseMinorVersion}>-</button>
+				<button onMouseDown={increaseMinorVersion}>+</button>
+			</div>
 		</header>
 	);
 }
