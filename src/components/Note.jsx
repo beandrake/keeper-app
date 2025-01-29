55c6bb578b9b83b32848af2c07889124d1d1a085
Branden Bean Drake <30214543+beandrake@users.noreply.github.com>
2025-01-29 11:21:40 -0800

    Added some hooks to dynamically change the styling of notes depending on mouse hovering.

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
 function Note(props) {
+	
+	const [noteState, setNoteState] = React.useState('default');
+
+	function setStateOver() {
+		setNoteState('hover');
+	}
+
+	function setStateOut() {
+		setNoteState('default');
+	}	
+	
+	const noteClasses = ['note', noteState].join(" ");
+
 	return (
-		<div className='note'>
+		<div className={noteClasses} onMouseOver={setStateOver} onMouseOut={setStateOut}>
 			<h1>{props.title}</h1>
 			<p>{props.content}</p>
 		</div>
