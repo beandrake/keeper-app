53ce43f9126fe45c3ae9cc17af296b8e4c157a27
Branden Bean Drake <30214543+beandrake@users.noreply.github.com>
2025-03-13 17:18:47 -0700

    Made the addNote functionality into a form because apparently that's the proper way, and added distinct styling for it.

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -10,7 +10,7 @@ function NoteInput(props) {
 	const contentRef = React.useRef("");
 
 
-	function AddNote() {
+	function addNote() {
 		// these current objects contain the whole tag and then some
 		const title = titleRef.current.value;
 		const content = contentRef.current.value;
@@ -18,21 +18,28 @@ function NoteInput(props) {
 		props.addNote(title, content);
 	}
 
+	function preventDefaultReload(event){
+		// The default behavior of a button in a form is to change the URL and reload the page.
+		// We do not want this.
+		event.preventDefault();
+	}
+
 	return (
-		<div className="noteInput">
-			<p>Create a New Note</p>
+		<form className="noteInput" onSubmit={preventDefaultReload}>
 			<input
+				className="title"
 				type="text"
 				ref={titleRef}
 				placeholder={titlePlaceholder}
 			/>
-			<input
+			<textarea
+				className="content"
 				type="text"
 				ref={contentRef}
 				placeholder={contentPlaceholder}
 			/>
-			<button onMouseDown={AddNote}>Add Note</button>
-		</div>
+			<button onMouseDown={addNote}>Add</button>
+		</form>
 	);
 }
 
